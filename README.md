6b88efac441d2bc34e274b1b0fb620b3d2dbf6ae
user <user@gray.lan>
2025-06-24 19:55:23 -0500

    Add docs/ subdir for github pages

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# C Xorshift PRNG library (cxsrandlib)
+# C Xorshift Seeded PRNG library (cxsrandlib)
 cxsrandlib is a C xorshift seeded pseudorandom number generator library with minimal dependencies. It provides integral, floating point, and normal distribution interfaces to the underlying PRNG implementation.
 
 # API Documentation
---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['cxsrandlib_5fdouble_0',['cxsrandlib_double',['../cxsrandlib_8h.html#a67e1d3c31659ae8ebfbd6888bdcbc44b',1,'cxsrandlib.h']]],
+  ['cxsrandlib_5fi32_5frange_1',['cxsrandlib_i32_range',['../cxsrandlib_8h.html#a1ef7f0c0d2362a4fc6d28788b7e457de',1,'cxsrandlib.h']]],
+  ['cxsrandlib_5fnormal_2',['cxsrandlib_normal',['../cxsrandlib_8h.html#a69202c9aa776df03f8b7e28ebe08f401',1,'cxsrandlib.h']]],
+  ['cxsrandlib_5fnormal2_3',['cxsrandlib_normal2',['../cxsrandlib_8h.html#a57cb5e93408e362f90ed852eb1a85df6',1,'cxsrandlib.h']]],
+  ['cxsrandlib_5fu32_4',['cxsrandlib_u32',['../cxsrandlib_8h.html#aaa931619ee9ed2a64c7a9c9722fd66e8',1,'cxsrandlib.h']]],
+  ['cxsrandlib_5fu32_5frange_5',['cxsrandlib_u32_range',['../cxsrandlib_8h.html#a9dbb88a4903a09b59d6585319c916817',1,'cxsrandlib.h']]]
+];
