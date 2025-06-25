899a7a07887ea973aa64e7ebc97c9749a2ecb894
user <user@gray.lan>
2025-06-24 19:47:48 -0500

    Initial public release 	new file:   Doxyfile 	new file:   LICENSE 	new file:   README.md 	new file:   gendoc 	new file:   src/cxsrandlib.c 	new file:   src/cxsrandlib.h 	new file:   test/Makefile 	new file:   test/cxsrandtest.c 	new file:   test/probdata/normal.csv 	new file:   test/probdata/normal.ods 	new file:   test/probdata/uniform-0-99.csv 	new file:   test/probdata/uniform-0-99.ods 	new file:   test/run

---
+++ README.md
@@ -0,0 +1,8 @@
+# C Xorshift PRNG library (cxsrandlib)
+cxsrandlib is a C xorshift seeded pseudorandom number generator library with minimal dependencies. It provides integral, floating point, and normal distribution interfaces to the underlying PRNG implementation.
+
+# API Documentation
+See:
+
+# License
+Licensed under the BSD-3 License
---
+++ test/cxsrandtest.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <assert.h>
+
+#include "cxsrandlib.h"
+
+#define Epsilon 0.000001
+
+int main(int argc, char **argv)
+{
+    uint32_t state = (uint32_t)time(NULL);
+
+    if(argc < 2)
+    {
+        // i32 bounds tests
+        assert(cxsrandlib_i32_range(-1, -2, 0, &state) == -1);
+        assert(cxsrandlib_i32_range(-1, -1, 0, &state) == -1);
+        assert(cxsrandlib_i32_range(-1, 0, 0, &state) == -1);
+        assert(cxsrandlib_i32_range(0, -1, 0, &state) == 0);
+        assert(cxsrandlib_i32_range(0, 0, 0, &state) == 0);
+        assert(cxsrandlib_i32_range(0, 1, 0, &state) == 0);
+        assert(cxsrandlib_i32_range(4, 3, 0, &state) == 4);
+        assert(cxsrandlib_i32_range(4, 4, 0, &state) == 4);
+        assert(cxsrandlib_i32_range(4, 5, 0, &state) == 4);
+        assert((cxsrandlib_i32_range(0, 8, 0, &state)&~7) == 0);
+        assert((cxsrandlib_i32_range(0, 256, 0, &state)&~255) == 0);
+        assert(cxsrandlib_i32_range(INT32_MAX, INT32_MAX, 0, &state) == INT32_MAX);
+        assert(cxsrandlib_i32_range(INT32_MAX, INT32_MIN, 0, &state) == INT32_MAX);
+        assert(cxsrandlib_i32_range(INT32_MIN, INT32_MIN, 0, &state) == INT32_MIN);
+        assert(cxsrandlib_i32_range(200, -5, 0, &state) == 200);
+        assert(cxsrandlib_i32_range(-5, -7, 0, &state) == -5);
+
+        // u32 bounds tests
+        assert(cxsrandlib_u32_range(0, 0, 0, &state) == 0);
+        assert(cxsrandlib_u32_range(0, 1, 0, &state) == 0);
+        assert(cxsrandlib_u32_range(2, 0, 0, &state) == 2);
+        assert(cxsrandlib_u32_range(2, 1, 0, &state) == 2);
+        assert(cxsrandlib_u32_range(2, 2, 0, &state) == 2);
+        assert(cxsrandlib_u32_range(2, 3, 0, &state) == 2);
+        assert(cxsrandlib_u32_range(UINT32_MAX, 0, 0, &state) == UINT32_MAX);
+        assert(cxsrandlib_u32_range(UINT32_MAX, UINT32_MAX, 0, &state) == UINT32_MAX);
+
+        // i32 bounds tests (with xorbits)
+        assert(cxsrandlib_i32_range(-1, -2, 98, &state) == -1);
+        assert(cxsrandlib_i32_range(-1, -1, 12435, &state) == -1);
+        assert(cxsrandlib_i32_range(-1, 0, 231, &state) == -1);
+        assert(cxsrandlib_i32_range(0, -1, 0x18291fff, &state) == 0);
+        assert(cxsrandlib_i32_range(0, 0, 37, &state) == 0);
+        assert(cxsrandlib_i32_range(0, 1, 52, &state) == 0);
+        assert(cxsrandlib_i32_range(4, 3, 89, &state) == 4);
+        assert(cxsrandlib_i32_range(4, 4, 110, &state) == 4);
+        assert(cxsrandlib_i32_range(4, 5, 2354, &state) == 4);
+        assert((cxsrandlib_i32_range(0, 8, 0x7fffffff, &state)&~7) == 0);
+        assert((cxsrandlib_i32_range(0, 256, 789, &state)&~255) == 0);
+        assert(cxsrandlib_i32_range(INT32_MAX, INT32_MAX, 1, &state) == INT32_MAX);
+        assert(cxsrandlib_i32_range(INT32_MAX, INT32_MIN, 22, &state) == INT32_MAX);
+        assert(cxsrandlib_i32_range(INT32_MIN, INT32_MIN, 333, &state) == INT32_MIN);
+        assert(cxsrandlib_i32_range(200, -5, 4444, &state) == 200);
+        assert(cxsrandlib_i32_range(-5, -7, 5555, &state) == -5);
+
+        // u32 bounds tests (with xorbits)
+        assert(cxsrandlib_u32_range(0, 0, 3142199, &state) == 0);
+        assert(cxsrandlib_u32_range(0, 1, 0xc578f, &state) == 0);
+        assert(cxsrandlib_u32_range(2, 0, 235, &state) == 2);
+        assert(cxsrandlib_u32_range(2, 1, 124, &state) == 2);
+        assert(cxsrandlib_u32_range(2, 2, 0xffffffff, &state) == 2);
+        assert(cxsrandlib_u32_range(2, 3, 1, &state) == 2);
+        assert(cxsrandlib_u32_range(UINT32_MAX, 0, 88, &state) == UINT32_MAX);
+        assert(cxsrandlib_u32_range(UINT32_MAX, UINT32_MAX, 7, &state) == UINT32_MAX);
+
+        // double bounds testing
+        for(int ix = 0; ix < 100; ix++)
+        {
+            double d = cxsrandlib_double(&state);
+            assert(d >= 0.0 - Epsilon);
+            assert(d <= 1.0 + Epsilon);
+        }
+
+        // Open up OS randomness source
+        int fd = open("/dev/urandom", O_RDONLY);
+        assert(fd >= 0);
+
+        // i32 randomized testing
+        for(int ix = 0; ix < 100; ix++)
+        {
+            int32_t lo = 0, bound = 0;
+            uint32_t xorbits = 0;
+            assert(read(fd, &lo, sizeof lo) == sizeof lo);
+            assert(read(fd, &bound, sizeof bound) == sizeof bound);
+            assert(read(fd, &xorbits, sizeof xorbits) == sizeof xorbits);
+            int32_t i32 = cxsrandlib_i32_range(lo, bound, xorbits, &state);
+            assert(i32 >= lo);
+            if(bound > lo)
+                assert(i32 < bound);
+        }
+
+        // u32 randomized testing
+        for(int ix = 0; ix < 100; ix++)
+        {
+            uint32_t lo = 0, bound = 0, xorbits = 0;
+            assert(read(fd, &lo, sizeof lo) == sizeof lo);
+            assert(read(fd, &bound, sizeof bound) == sizeof bound);
+            assert(read(fd, &xorbits, sizeof xorbits) == sizeof xorbits);
+            uint32_t u32 = cxsrandlib_u32_range(lo, bound, xorbits, &state);
+            assert(u32 >= lo);
+            if(bound > lo)
+                assert(u32 < bound);
+        }
+
+        // Close fd
+        close(fd);
+    }
+
+    // Results generator for probability analysis
+    else for(int ix = 0; ix < 100; ix++)
+    {
+        switch(argv[1][0])
+        {
+        case 'u': printf("%u,\n", cxsrandlib_u32(&state)); break;
+        case 'r': printf("%d,\n", cxsrandlib_i32_range(0, 100, 0, &state)); break;
+        case 'd': printf("%f,\n", cxsrandlib_double(&state)); break;
+        case 'n': printf("%f,\n", cxsrandlib_normal(0.0, 1.0, &state)); break;
+        }
+    }
+
+    // Done
+    printf("cxsrandlibtest completed successfully\n");
+    exit(0);
+}
